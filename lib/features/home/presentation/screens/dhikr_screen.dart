490e594f5380f28fe8c3e07b63901320c932a58b
Sofiian <sofianemad98@gmail.com>
2025-09-28 16:20:55 +0300

    changed dhikr screen name

---
+++ lib/features/home/presentation/screens/dhikr_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DhikrScreen extends StatefulWidget {
+  const DhikrScreen({super.key});
+
+  @override
+  State<DhikrScreen> createState() => _DhikrScreenState();
+}
+
+class _DhikrScreenState extends State<DhikrScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
